219b0be79b9c9707d261c691dcb38bcdd80ba41b
brown <brown1@gmail.com>
2020-09-11 14:09:01 -0400

    Issues with pexpect

--- dcr_cmd.sh
+++ dcr_cmd.sh
@@ -1,7 +1,7 @@
 # List of Docker commands used:
 
 # Build command:
-docker build -t mmdev:v4 .
+docker build -t mmdev:latest .
 
 # Run command
-docker run -it -p 8000:8000 --name mmdev-v4 mmdev:v4
+docker run -it -p 8000:8000 --name mmdev mmdev:latest
